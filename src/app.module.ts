5e1d8ba8da0bb292b2ec99764dcfffd2a771dd5c
bapcai02 <hadv62@wru.vn>
2021-10-06 23:39:31 +0700

    table

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,25 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { type } from 'os';
 import { TagsModule } from './tags/tags.module';
+import { SocialsModule } from './socials/socials.module';
+import { SlidersModule } from './sliders/sliders.module';
+import { SellersModule } from './sellers/sellers.module';
+import { RolesModule } from './roles/roles.module';
+import { RatingsModule } from './ratings/ratings.module';
+import { ProductTagsModule } from './product-tags/product-tags.module';
+import { ProductImagesModule } from './product-images/product-images.module';
+import { ProductModule } from './product/product.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderDetailsModule } from './order-details/order-details.module';
+import { EmployerModule } from './employer/employer.module';
+import { CustomersModule } from './customers/customers.module';
+import { CouponsModule } from './coupons/coupons.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CartsModule } from './carts/carts.module';
+import { BrandsModule } from './brands/brands.module';
+import { QuanhuyenModule } from './quanhuyen/quanhuyen.module';
+import { TinhtpModule } from './tinhtp/tinhtp.module';
+import { XaphuongttrModule } from './xaphuongttr/xaphuongttr.module';
 
 @Module({
   imports: [
@@ -22,6 +41,25 @@ import { TagsModule } from './tags/tags.module';
     // eslint-disable-next-line prettier/prettier
     UsersModule,
     TagsModule,
+    SocialsModule,
+    SlidersModule,
+    SellersModule,
+    RolesModule,
+    RatingsModule,
+    ProductTagsModule,
+    ProductImagesModule,
+    ProductModule,
+    OrdersModule,
+    OrderDetailsModule,
+    EmployerModule,
+    CustomersModule,
+    CouponsModule,
+    CategoriesModule,
+    CartsModule,
+    BrandsModule,
+    QuanhuyenModule,
+    TinhtpModule,
+    XaphuongttrModule,
   ],
   controllers: [AppController],
   providers: [AppService],
