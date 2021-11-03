0e901b52ab88da32a395ab5b06d7925d42f31bae
hadv <hadv@2nf.com.vn>
2021-11-03 17:45:24 +0700

    Update users.create.dto.ts
    validate dto

--- src/dto/users.create.dto.ts
+++ src/dto/users.create.dto.ts
@@ -1,15 +1,23 @@
 /* eslint-disable prettier/prettier */
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, IsInt, Min, Max } from "class-validator";
 export class CreateUserDto {
-    @IsNotEmpty()
-    name: string;
 
     @IsNotEmpty()
+    @IsInt()
+    @Min(1)
+    @Max(4)
     role_id: number;
+
+    @IsNotEmpty()
+    @Min(1)
+    @Max(191)
+    name: string;
 
     @IsEmail()
     email: string;
 
     @IsNotEmpty()
+    @Min(5)
+    @Max(20)
     password: string;
   }
